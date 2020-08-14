b78c1fa431fef9c3d872f268d728cd680e8d2498
Kyle Stough <kstough00@gmail.com>
2020-08-14 17:46:10 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,7 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def get_response_body
-   uri = URI.parse(URL)
+    uri = URI.parse(URL)
     uri.open.string
   end
   
